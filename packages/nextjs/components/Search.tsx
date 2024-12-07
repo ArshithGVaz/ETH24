4ec5d24e8bb36374400b8ae7ad0f27ca09d0ad72
vyshnav <vyshnavravi599@gmail.com>
2024-12-07 16:47:24 +0530

    refactor search functionality in SearchBar component

--- packages/nextjs/components/Search.tsx
+++ packages/nextjs/components/Search.tsx
@@ -1,16 +1,22 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { PlaceholdersAndVanishInput } from "./ui/placeholder-and-vanish-input";
 
 export function SearchBar() {
   const placeholders = ["Beachesgit  in Coastal Karnataka", "Forts in Hampi", "View Points in Coorg"];
+  const [searchTerm, setSearchTerm] = useState("");
+  const router = useRouter();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
+    setSearchTerm(e.target.value);
   };
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("submitted");
+    e.preventDefault();
+    const search = searchTerm.trim().replace(/\s+/g, ""); // Trim spaces and remove all spaces
+    router.push(`/search/${search}`);
   };
   return (
     <div className="h-[40rem] flex flex-col justify-center  items-center px-4">
